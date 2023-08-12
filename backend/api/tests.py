9b5c9143d70ae0b88f79752f742139d211e61762
Max-ubuntu <79164211428@yandex.ru>
2023-08-12 17:07:54 +0300

    Add Actions

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,11 +1,13 @@
-# backend/api/tests.py
 from http import HTTPStatus
 
 from api import models
 from django.test import Client, TestCase
 
+
 class TaskiAPITestCase(TestCase):
+
     def setUp(self):
+
         self.guest_client = Client()
 
     def test_list_exists(self):
